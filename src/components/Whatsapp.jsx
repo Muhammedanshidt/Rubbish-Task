f579523ab4f289954aa9853802065d8e957d717a
Anshid <muhammedanshidh.t@gmail.com>
2025-01-17 09:40:24 +0530

    third

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -10,7 +10,7 @@ const WhatsAppButton = ({custom}) => {
     <button
       onClick={handleChat}
       
-      className={`${custom ? custom : "fixed bottom-4 right-32 bg-green-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-green-600"}`}
+      className={`${custom ? custom : "fixed bottom-4 right-4 bg-green-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-green-600"}`}
     >
       Chat Now
     </button>
